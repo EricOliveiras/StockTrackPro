a17feffe26c26dc6631de247ff6b4214952054e8
Eric Nascimento <oiericoi@hotmail.com>
2024-03-25 19:33:24 -0300

    first commit

---
+++ src/main/java/com/ericoliveiras/StockTrackPro/StockTrackProApplication.java
@@ -0,0 +1,13 @@
+package com.ericoliveiras.StockTrackPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericoliveiras/StockTrackPro/StockTrackProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericoliveiras.StockTrackPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
