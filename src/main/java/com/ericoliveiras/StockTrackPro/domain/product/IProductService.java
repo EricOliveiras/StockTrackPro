eaf41d6893f511cf71e7a9208d9c8834200d12f2
Eric Nascimento <oiericoi@hotmail.com>
2024-03-26 16:16:20 -0300

    feat: add product service interface

---
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/product/IProductService.java
@@ -0,0 +1,14 @@
+package com.ericoliveiras.StockTrackPro.domain.product;
+
+import com.ericoliveiras.StockTrackPro.domain.product.payload.request.CreateProductRequest;
+import com.ericoliveiras.StockTrackPro.domain.product.payload.response.ProductResponse;
+
+import java.util.List;
+
+public interface IProductService {
+    ProductResponse create(CreateProductRequest createProductRequest);
+
+    ProductResponse find(Long id);
+
+    List<ProductResponse> findAll();
+}
