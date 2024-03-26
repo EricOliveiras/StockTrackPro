efce19506a3bc3648045e44ae4306132d238c021
Eric Nascimento <oiericoi@hotmail.com>
2024-03-26 16:13:41 -0300

    feat: add create product request

---
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/product/payload/request/CreateProductRequest.java
@@ -0,0 +1,40 @@
+package com.ericoliveiras.StockTrackPro.domain.product.payload.request;
+
+public class CreateProductRequest {
+    private String name;
+    private String description;
+    private Double price;
+    private int quantityInStock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+}
