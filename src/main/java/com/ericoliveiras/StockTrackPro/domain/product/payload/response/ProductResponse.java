49185daf882675da2612b753145938e8671f4d8a
Eric Nascimento <oiericoi@hotmail.com>
2024-03-26 16:14:06 -0300

    feat: add product response

---
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/product/payload/response/ProductResponse.java
@@ -0,0 +1,72 @@
+package com.ericoliveiras.StockTrackPro.domain.product.payload.response;
+
+import java.util.Date;
+
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private int quantityInStock;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public ProductResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
