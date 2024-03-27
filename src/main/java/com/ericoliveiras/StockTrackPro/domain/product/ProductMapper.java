0fd4dc9d399087c10d20e8f65bd8c75c50754d7c
Eric Nascimento <oiericoi@hotmail.com>
2024-03-27 15:09:34 -0300

    refactor: modify product mapper

--- src/main/java/com/ericoliveiras/StockTrackPro/domain/product/ProductMapper.java
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/product/ProductMapper.java
@@ -2,9 +2,11 @@ package com.ericoliveiras.StockTrackPro.domain.product;
 
 import com.ericoliveiras.StockTrackPro.domain.product.payload.request.CreateProductRequest;
 import com.ericoliveiras.StockTrackPro.domain.product.payload.response.ProductResponse;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
     Product toEntity(CreateProductRequest createProductRequest);
     ProductResponse toDto(Product product);
