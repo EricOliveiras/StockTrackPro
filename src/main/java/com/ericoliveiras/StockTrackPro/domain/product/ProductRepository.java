2f3e68ff808bdcbff7c9665d2a5fa733baab0d0a
Eric Nascimento <oiericoi@hotmail.com>
2024-03-27 15:08:39 -0300

    feat: add find by name method in product repository

--- src/main/java/com/ericoliveiras/StockTrackPro/domain/product/ProductRepository.java
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/product/ProductRepository.java
@@ -1,6 +1,11 @@
 package com.ericoliveiras.StockTrackPro.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %?1%")
+    List<Product> findByName(String name);
 }
