c7860941992c5443e33ba0e2bf5fc5ecaa01e6ec
Eric Nascimento <oiericoi@hotmail.com>
2024-03-29 14:16:16 -0300

    refactor: modify product service impl

--- src/main/java/com/ericoliveiras/StockTrackPro/domain/product/ProductServiceImpl.java
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/product/ProductServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ericoliveiras.StockTrackPro.domain.product;
 
+import com.ericoliveiras.StockTrackPro.config.error.CustomException;
 import com.ericoliveiras.StockTrackPro.domain.product.payload.request.CreateProductRequest;
 import com.ericoliveiras.StockTrackPro.domain.product.payload.response.ProductResponse;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,6 +29,7 @@ public class ProductServiceImpl implements IProductService {
     @Override
     public ProductResponse create(CreateProductRequest createProductRequest) {
         Product product = productMapper.toEntity(createProductRequest);
+        findByName(product.getName());
         product.setName(product.getName().toLowerCase());
         return productMapper.toDto(productRepository.save(product));
     }
@@ -36,8 +40,8 @@ public class ProductServiceImpl implements IProductService {
     }
 
     @Override
-    public List<ProductResponse> findByName(String name) {
-        return productMapper.toDto(findProductByName(name));
+    public List<ProductResponse> findAllByName(String name) {
+        return productMapper.toDto(findProductsByName(name));
     }
 
     @Override
@@ -51,20 +55,35 @@ public class ProductServiceImpl implements IProductService {
                 collect(Collectors.toList());
     }
 
+    @Override
+    public ProductResponse update(Long id, CreateProductRequest createProductRequest) {
+        Product product = findProduct(id);
+        BeanUtils.copyProperties(createProductRequest, product, "id");
+        product.setName(createProductRequest.getName().toLowerCase());
+        return productMapper.toDto(productRepository.save(product));
+    }
+
     private Product findProduct(long id) {
         return productRepository.
                 findById(id).
                 orElseThrow(
-                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "product not found.")
+                        () -> new CustomException("product not found.", HttpStatus.NOT_FOUND)
                 );
     }
 
 
-    private @NotNull List<Product> findProductByName(@NotNull String name) {
-        List<Product> products = productRepository.findByName(name.toLowerCase());
+    private @NotNull List<Product> findProductsByName(@NotNull String name) {
+        List<Product> products = productRepository.findAllByName(name.toLowerCase());
         if (products.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "products not found.");
+            throw new CustomException("products not found.", HttpStatus.NOT_FOUND);
         }
         return products;
+    }
+
+    private void findByName(String name) {
+        Product product = productRepository.findByName(name.toLowerCase());
+        if (product != null) {
+            throw new CustomException("there is already a product registered with this name.", HttpStatus.CONFLICT);
+        }
     }
 }
