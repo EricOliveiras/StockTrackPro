a5147b508b1f58bb057f07475211800e6c580eb3
Eric Nascimento <oiericoi@hotmail.com>
2024-04-05 20:54:50 -0300

    feat: add sell repository

---
+++ src/main/java/com/ericoliveiras/StockTrackPro/domain/sell/SellRepository.java
@@ -0,0 +1,6 @@
+package com.ericoliveiras.StockTrackPro.domain.sell;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellRepository extends JpaRepository<Sell, Long> {
+}
